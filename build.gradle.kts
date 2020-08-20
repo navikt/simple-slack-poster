c5d28d0f83c84b86bdee65c702ab28c2ad04d94f
Richard Borge <richard.borge@nav.no>
2020-08-20 21:24:40 +0200

    Creating simple slack poster library

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+    `java-library`
+    `maven-publish`
+}
+
+val githubUser: String by project
+val githubPassword: String by project
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/simple-slack-poster")
+        credentials {
+            username = githubUser
+            password = githubPassword
+        }
+    }
+}
+
+version = properties["version"] ?: "local"
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.bazaarvoice.jackson:rison:2.9.10.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.11.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.2")
+    implementation("org.slf4j:slf4j-api:1.7.30")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/btn-common")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            pom {
+                name.set("simple-slack-poster")
+                description.set("Library for posting messages to Slack")
+                url.set("https://github.com/navikt/simple-slack-poster")
+                groupId = "no.nav.slackposter"
+                artifactId = "simple-slack-poster"
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/navikt/simple-slack-poster.git")
+                    developerConnection.set("scm:git:https://github.com/navikt/simple-slack-poster.git")
+                    url.set("https://github.com/navikt/simple-slack-poster")
+                }
+            }
+            from(components["java"])
+        }
+    }
+}
---
+++ src/main/kotlin/no/nav/slackposter/SlackClient.kt
@@ -0,0 +1,78 @@
+package no.nav.slackposter
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.slf4j.LoggerFactory
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+
+enum class Severity(val emoji: String) {
+    ERROR(":scream:"),
+    WARN(":thinking_face"),
+    INFO(":information_source:")
+}
+
+class SlackClient(
+        private val webhookUrl: String,
+        private val channel: String,
+        private val application: String,
+        private val clusterName: String
+) {
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = LoggerFactory.getLogger(javaClass.enclosingClass)
+        private val objectMapper = jacksonObjectMapper()
+                .registerModule(JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    }
+
+    fun postMessage(text: String, severity: Severity = Severity.INFO) {
+        webhookUrl.post(objectMapper.writeValueAsString(mutableMapOf<String, Any>(
+                "channel" to channel,
+                "username" to "$application ($clusterName)",
+                "text" to text,
+                "icon_emoji" to severity.emoji
+        )))
+    }
+
+    private fun String.post(jsonPayload: String): String? {
+        var connection: HttpURLConnection? = null
+        try {
+            connection = (URL(this).openConnection() as HttpURLConnection).apply {
+                requestMethod = "POST"
+                connectTimeout = 5000
+                readTimeout = 5000
+                doOutput = true
+                setRequestProperty("Content-Type", "application/json; charset=utf-8")
+
+                outputStream.use { it.bufferedWriter(Charsets.UTF_8).apply { write(jsonPayload); flush() } }
+            }
+
+            val responseCode = connection.responseCode
+
+            if (connection.responseCode !in 200..299) {
+                logger.warn("response from slack: code=$responseCode")
+                return null
+            }
+
+            val responseBody = connection.inputStream.readText()
+            logger.debug("response from slack: code=$responseCode")
+
+            return responseBody
+        } catch (err: SocketTimeoutException) {
+            logger.warn("timeout waiting for reply", err)
+        } catch (err: IOException) {
+            logger.error("error posting to slack: {}", err.message, err)
+        } finally {
+            connection?.disconnect()
+        }
+
+        return null
+    }
+
+    private fun InputStream.readText() = use { it.bufferedReader().readText() }
+}
