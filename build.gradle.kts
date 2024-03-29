b3a00530ed8c7c0af787110a8491ea1305a7fab4
Andreas Jonsson <andreas.jonsson@nav.no>
2024-03-29 10:03:39 +0100

    Java 21. Update GH action. Gradle 8.5.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 group = "no.nav.slackposter"
 version = properties["version"] ?: "local-build"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
@@ -26,7 +26,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions{
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
